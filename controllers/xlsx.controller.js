3614155b39ee1ca288e17523161d93dc0ba9304d
Marek <43797932+lasagnu@users.noreply.github.com>
2018-11-27 03:54:46 +0100

    Update xlsx.controller.js

--- controllers/xlsx.controller.js
+++ controllers/xlsx.controller.js
@@ -4,7 +4,6 @@ import logger from '../core/logger/app-logger'
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-// returns letter and number of the cell as array of 2 values
 function splitCellIdent(text) {
     let letter = text.substring(0, 1);
     let number = text.substring(1);
@@ -26,8 +25,6 @@ function fetchCellValue(charIndex, number, worksheet){
   }
 }
 
-
-// function that reads single cell from file
 export function readCell( file_path, cell ) {
 
     let workbook = XLSX.readFile(file_path);
@@ -39,7 +36,6 @@ export function readCell( file_path, cell ) {
     return desiredValue;
 }
 
-// function that read multiple values from file, ex: A1:B34
 export async function readCells( file_path, range ) {
 
     let workbook = await XLSX.readFile(file_path);
@@ -47,13 +43,12 @@ export async function readCells( file_path, range ) {
     let worksheet = workbook.Sheets[firstSheetName];
     let output = '';
 
-    //input: 'A1:B3'
+
     let valueRangeArray = range.split(':');
-    //A1, B3
+
     let beginRange = splitCellIdent(valueRangeArray[0]);
-    // {A, 1}, B3
+
     let endRange = splitCellIdent(valueRangeArray[1]);
-    // {A, 1}, {B, 3}
 
     let beginLetterIndex = alphabet.indexOf(beginRange[0].toLowerCase());
     let beginIndex = beginRange[1];
